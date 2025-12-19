9693277dba47c367d19bbb8ac5a8ea0b2ca48735
Elazar Pimentel <elazar.pimentel@gmail.com>
2025-12-19 11:13:29 -0300

    2025-12-19-1113

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,12 @@
 import { StrictMode } from 'react'
-import { createRoot, hydrateRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './Routes.jsx'
 
-const rootElement = document.getElementById('root')
-
-if (rootElement.hasChildNodes()) {
-  // Hydrate for react-snap prerendered content
-  hydrateRoot(
-    rootElement,
-    <StrictMode>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </StrictMode>
-  )
-} else {
-  // Normal render for development
-  createRoot(rootElement).render(
-    <StrictMode>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </StrictMode>
-  )
-}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  </StrictMode>
+)
