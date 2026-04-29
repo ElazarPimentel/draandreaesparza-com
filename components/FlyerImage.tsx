e9c62633a4ebdfd89838a5975512d4016a033319
Elazar Pimentel <elazar.pimentel@gmail.com>
2026-04-29 20:54:08 -0300

    2026-04-29-2054

--- components/FlyerImage.tsx
+++ components/FlyerImage.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useId, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 interface FlyerImageProps {
   src: string
@@ -11,7 +11,6 @@ interface FlyerImageProps {
 
 export default function FlyerImage({ src, alt, width, height }: FlyerImageProps) {
   const [isOpen, setIsOpen] = useState(false)
-  const titleId = useId()
 
   useEffect(() => {
     if (!isOpen) return
@@ -40,17 +39,14 @@ export default function FlyerImage({ src, alt, width, height }: FlyerImageProps)
 
       {isOpen && (
         <div className="image-modal-backdrop" role="presentation" onClick={() => setIsOpen(false)}>
-          <div className="image-modal" role="dialog" aria-modal="true" aria-labelledby={titleId} onClick={event => event.stopPropagation()}>
-            <div className="image-modal-header">
-              <h2 id={titleId}>Vista ampliada</h2>
-              <button type="button" className="image-modal-close" onClick={() => setIsOpen(false)}>
-                Cerrar
-              </button>
-            </div>
+          <div className="image-modal" role="dialog" aria-modal="true" aria-label="Imagen ampliada" onClick={event => event.stopPropagation()}>
             <div className="image-modal-body">
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img src={src} alt={alt} width={width} height={height} />
             </div>
+            <button type="button" className="image-modal-close" onClick={() => setIsOpen(false)}>
+              Cerrar
+            </button>
           </div>
         </div>
       )}
